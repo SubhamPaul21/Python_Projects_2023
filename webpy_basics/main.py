e45810121063a81b0323512d9b312cf776f94f79
SubhamPaul21 <subhampaul12@gmail.com>
2023-11-21 17:23:47 +0530

    Pass values in url with webpy

--- webpy_basics/main.py
+++ webpy_basics/main.py
@@ -1,17 +1,20 @@
 import web
 
 urls = (
-    '/(.*)', 'Index'
+    '/(.*)/(.*)', 'Index'
 )
 
+render = web.template.render('./resources')
 app = web.application(urls, globals())
 
 
 class Index:
-    def GET(self, name):
+    def GET(self, name, age):
         if not name:
             name = "World"
-        return "Hello " + name.capitalize() + ". Hope you are doing well!"
+        if not age:
+            age = 26
+        return render.main(name, age)
 
 
 if __name__ == "__main__":
